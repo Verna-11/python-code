9a387680149f28d7418dddf6c6ec772fae80df42
John Vernie <vernardo.barrera28@gmail.com>
2025-06-17 04:17:25 -0400

    Simple string formatting

---
+++ stringf.py
@@ -0,0 +1,9 @@
+#python 3.12
+
+#simple string formatting in python
+
+MyName = 'Verna' # string
+MyAge = 30 # integer
+
+#string formatting
+print(f'My name is {Myname} and My age is {MyAge}') # we can't concatenate string and integer
