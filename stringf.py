dcfcfc0a15d464b11a95ab6631d247b46a47cd7c
Verna-11 <vernardo.barrera28@gmail.com>
2025-06-30 08:43:55 -0400

    Update stringf.py

--- stringf.py
+++ stringf.py
@@ -1,9 +1,9 @@
 #python 3.12
 
-#simple string formatting in python
+#simple f-string formatting in python
 
-MyName = 'Verna' # string
-MyAge = 30 # integer
+MyName = 'Verna11' # string
+MyAge = 26 # integer
 
 #string formatting
 print(f'My name is {Myname} and My age is {MyAge}') # we can't concatenate string and integer
